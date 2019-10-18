a656a766f9c59d457900af97478868833364495a
cfowler650 <cfowler650@gmail.com>
2019-10-18 16:31:59 -0500

    added login and create account conditional render home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -67,6 +67,8 @@ class PostForm extends React.Component {
             username: '',
             name: '',
             password_digest: '',
+            newUser: true
+
         }
     }
 
@@ -85,11 +87,29 @@ class PostForm extends React.Component {
             })
     }
 
+    submitLoginHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('/api/users', this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    handleUserStatus = (e) => {
+        e.preventDefault()
+        this.setState({ newUser: !this.state.newUser })
+    }
+
     render() {
         const { email, username, name, password_digest } = this.state
         return (
             <div className="bg-text" style={{ display: "flex", flexDirection: "column" }}>
-                <div>
+
+                {this.state.newUser ? <div>
 
                     <h2 className="sign-up-header">Create an account</h2>
 
@@ -135,17 +155,57 @@ class PostForm extends React.Component {
                         </div>
 
 
-                        <p>Already a member? Click here.</p>
+                        <p>Already a member? <a onClick={this.handleUserStatus}>Click here.</a></p>
                     </form>
 
-
-
-
-
-
-
-
                 </div>
+
+                    :
+
+                    <div>
+
+
+                        <h2 className="sign-up-header">Login</h2>
+
+                        <form className="login-container" onSubmit={this.submitLoginHandler}>
+
+                            <div style={{ width: "80%" }}>
+                                <h5 className="email-h5">username</h5>
+                                <input className="auth-input-field" type="text" name="username" value={username} onChange={this.changeHandler} />
+                            </div>
+
+                            <div style={{ width: "80%" }}>
+                                <h5 className="email-h5">password</h5>
+                                <input className="auth-input-field" type="text" name="password_digest" value={password_digest} onChange={this.changeHandler} />
+                            </div>
+                            <button style={{
+                                padding: "3%",
+                                background: "darkblue",
+                                width: "80%",
+                                border: "none",
+                                color: "white",
+                                fontSize: "16px",
+                                fontWeight: "500",
+                                fontFamily: "Hind",
+                                marginTop: "10px"
+
+
+                            }} type="submit"> Continue </button>
+
+
+                            <div className="guest-button" style={{ width: "80%" }}>
+                                <Link to={`/restaurant_page`} style={{
+                                    padding: "4%",
+                                    background: "#222",
+                                    width: "100%"
+                                }}>Continue as guest...</Link>
+                            </div>
+
+
+                            <p>Not a member? <a onClick={this.handleUserStatus}> Click here to sign up. </a></p>
+                        </form>
+
+                    </div>}
             </div>
         );
 
